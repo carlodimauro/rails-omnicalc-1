2473f3695d2d6bdfeb36103f0eef6fefdb6d702b
carlodimauro <146589712+carlodimauro@users.noreply.github.com>
2023-10-31 21:53:31 +0000

    onehunetpercent

--- app/controllers/omnicalc_controller.rb
+++ app/controllers/omnicalc_controller.rb
@@ -25,4 +25,36 @@ class OmnicalcController < ApplicationController
 
   end  
 
+  def compute
+    @function = params.fetch(:function)
+
+    if @function == "square"
+      @number = params.fetch(:number).to_f
+      @result = @number ** 2
+      render({:template => "/sqresult"})
+    elsif @function == "square_root"
+      @number = params.fetch(:number).to_f
+      @result = @number ** 0.5
+      render({:template => "/rootresult"})
+    elsif @function == "payment"
+      @apr = params.fetch(:user_apr).to_f
+      @yrs = params.fetch(:user_years).to_f
+      @pv = params.fetch(:user_pv).to_f  
+      @result = ((@apr/1200) * @pv) / (1 - ((1 + (@apr/1200))**(@yrs* -12.0)))
+      # formatt for output
+      @result = @result.to_fs(:currency)
+      @pv = @pv.to_fs(:currency)
+      @apr = sprintf("%.4f%%",@apr)
+      
+      render({:template => "/paymentresult"})
+    
+    elsif @function == "random"
+      @min = params.fetch(:user_min).to_f
+      @max = params.fetch(:user_max).to_f
+      @result = rand(@min..@max)
+      render({:template => "/randresult"})
+    
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ get("/square_root/new", {:controller => "omnicalc", :action => "squareroot" })
 get("/payment/new", {:controller => "omnicalc", :action => "payment" })
 get("/random/new", {:controller => "omnicalc", :action => "random" })
 
+get("/:function/results",  {:controller => "omnicalc", :action => "compute" })
 
 end
